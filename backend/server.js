51b29f14690585d3e8169d80b7a070e8fe91718f
mayank arya <mayanksunny786@gmail.com>
2026-05-03 11:54:49 +0530

    env changed

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,8 @@ const io = socketIo(server, {
 });
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 const authenticateToken = (req, res, next) => {
